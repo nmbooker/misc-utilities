4619497a51e80f6fec3d4bdebc5b9720b1dcdd21
Nick Booker <nmb@nickbooker.uk>
2016-01-10 18:15:30 +0000

    Added deps for os-backup-btrfs

--- deps.sh
+++ deps.sh
@@ -3,3 +3,4 @@
 sudo apt-get -y install liblinux-inotify2-perl libmail-box-perl libset-scalar-perl
 sudo apt-get -y install aptitude
 sudo apt-get -y install libnotify-bin
+sudo apt-get -y install libdatetime-perl liblockfile-simple-perl btrfs-tools
