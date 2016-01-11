6f562ca2f5ae4f3595a6c445ffd1d43a1a60efe6
Nick Booker <nmb@nickbooker.uk>
2016-01-11 21:14:41 +0000

    Do incremental backups, with monthly full

--- deps.sh
+++ deps.sh
@@ -3,4 +3,4 @@
 sudo apt-get -y install liblinux-inotify2-perl libmail-box-perl libset-scalar-perl
 sudo apt-get -y install aptitude
 sudo apt-get -y install libnotify-bin
-sudo apt-get -y install libdatetime-perl liblockfile-simple-perl btrfs-tools
+sudo apt-get -y install dar par2 liblockfile-simple-perl libdatetime-perl libtry-tiny-perl liblist-moreutils-perl liblist-utilsby-perl libfile-slurp-perl btrfs-tools
