87745c6349d0c04865c681cd89211acd3a794375
Nick Booker <nmb@nickbooker.uk>
2016-01-12 20:21:15 +0000

    Reap unused dependencies

--- deps.sh
+++ deps.sh
@@ -10,4 +10,4 @@ sudo apt-get -y install liblinux-inotify2-perl libmail-box-perl libset-scalar-pe
 sudo apt-get -y install aptitude
 
 # For os-backup-btrfs
-sudo apt-get -y install dar par2 liblockfile-simple-perl libdatetime-perl libtry-tiny-perl liblist-moreutils-perl liblist-utilsby-perl libfile-slurp-perl btrfs-tools libset-scalar-perl libcwd-guard-perl
+sudo apt-get -y install dar par2 liblockfile-simple-perl libdatetime-perl libtry-tiny-perl libfile-slurp-perl btrfs-tools libset-scalar-perl libcwd-guard-perl
