2ac36c3dcf25827a7f464eddd2d76d7471de4471
Nick Booker <nmb@nickbooker.uk>
2016-01-12 20:07:10 +0000

    Update dependencies script

--- deps.sh
+++ deps.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-sudo apt-get -y install liblinux-inotify2-perl libmail-box-perl libset-scalar-perl
+# for mboxnotify
+sudo apt-get -y install liblinux-inotify2-perl libmail-box-perl libset-scalar-perl libnotify-bin
+
+# for apt-manually-installed
 sudo apt-get -y install aptitude
-sudo apt-get -y install libnotify-bin
-sudo apt-get -y install dar par2 liblockfile-simple-perl libdatetime-perl libtry-tiny-perl liblist-moreutils-perl liblist-utilsby-perl libfile-slurp-perl btrfs-tools
+
+# For os-backup-btrfs
+sudo apt-get -y install dar par2 liblockfile-simple-perl libdatetime-perl libtry-tiny-perl liblist-moreutils-perl liblist-utilsby-perl libfile-slurp-perl btrfs-tools libset-scalar-perl
