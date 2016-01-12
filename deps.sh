fe805fa3bb8e04a54e4532c5e2ced9f5c33b593b
Nick Booker <nmb@nickbooker.uk>
2016-01-12 20:17:29 +0000

    Use Cwd::Guard to change directory

--- deps.sh
+++ deps.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# most perl scripts use this
+sudo apt-get -y install libgetopt-long-perl libpod-usage-perl
+
 # for mboxnotify
 sudo apt-get -y install liblinux-inotify2-perl libmail-box-perl libset-scalar-perl libnotify-bin
 
@@ -7,4 +10,4 @@ sudo apt-get -y install liblinux-inotify2-perl libmail-box-perl libset-scalar-pe
 sudo apt-get -y install aptitude
 
 # For os-backup-btrfs
-sudo apt-get -y install dar par2 liblockfile-simple-perl libdatetime-perl libtry-tiny-perl liblist-moreutils-perl liblist-utilsby-perl libfile-slurp-perl btrfs-tools libset-scalar-perl
+sudo apt-get -y install dar par2 liblockfile-simple-perl libdatetime-perl libtry-tiny-perl liblist-moreutils-perl liblist-utilsby-perl libfile-slurp-perl btrfs-tools libset-scalar-perl libcwd-guard-perl
